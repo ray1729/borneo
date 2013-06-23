ea896aede3464c60d8c592d9dd905347b9c9f63b
Ray Miller <ray@1729.org.uk>
2013-06-23 18:22:26 +0100

    Update project.clj for forked release

--- project.clj
+++ project.clj
@@ -1,13 +1,8 @@
-(defproject borneo "0.3.0" 
+(defproject org.clojars.ray1729/borneo "0.3.1-SNAPSHOT" 
   :description "Clojure wrapper for Neo4j, a graph database."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.neo4j/neo4j "1.4"]]
-  :dev-dependencies [[org.clojars.rayne/autodoc "0.8.0-SNAPSHOT"]]
-  :jvm-opts ["-Dswank.encoding=utf-8"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.neo4j/neo4j "1.9"]]
+  :profiles {:dev {:dependencies [[me.raynes/fs "1.4.5"]]}}
   :autodoc {:copyright "Copyright (C) 2011, Jozef Wagner. All rights reserved."
             :web-src-dir "http://github.com/wagjo/borneo/blob/"
             :web-home "http://wagjo.github.com/borneo/"})
-
-;;; you should install swank-clojure and lein-clojars locally:
-;; lein plugin install swank-clojure 1.3.2
-;; lein plugin install lein-clojars  0.7.0
